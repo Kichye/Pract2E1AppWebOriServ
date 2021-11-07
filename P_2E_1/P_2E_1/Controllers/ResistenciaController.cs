1c60272fe3dfe22e1bf6a9126db511b52ff002a5
Juan Cime <92643359+Kichye@users.noreply.github.com>
2021-11-07 14:37:53 -0600

    Add files via upload

---
+++ P_2E_1/P_2E_1/Controllers/ResistenciaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using P_2E_1.Services;
+using P_2E_1.Domain;
+
+namespace P_2E_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResistenciaController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult CalR(Resistencia rstc)
+
+        {
+            var rser = new ResistenciaService();
+            var res = rser.CalR(rstc.band1, rstc.band2, rstc.band3, rstc.band4);
+            return Ok(res);
+        }
+    }
+}
---
+++ P_2E_1/P_2E_1/Domain/Resistencia.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace P_2E_1.Domain
+{
+    public record Resistencia (string band1, string band2, string band3, string band4);
+}
